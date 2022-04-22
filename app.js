d640cff3346a931655b446be361a1f6d778be09f
Davey <daveyzaal@hotmail.nl>
2022-04-22 10:39:19 +0200

    connection mongo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+require('./controllers/connection')
 const express = require('express');
 const app = express();
 const mongoose = require(`mongoose`);
@@ -7,25 +7,32 @@ const uri = process.env.DB_HOST;
 const port = 3000;
 
 // const user = require('../models/user.js');
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true,}) .then(() => {
-    console.log('Verbonden met DB.')},
-);
+// mongoose.connect(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }).then(() => {
+//   console.log('Verbonden met DB.')
+// }, );
 
-var conn = mongoose.connection;
-conn.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// var conn = mongoose.connection;
+// conn.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // GET method route
 app.get('/', (req, res) => {
-    res.send('GET request to the homepage')
-    })
+  res.send('GET request to the homepage')
+})
 
-  // POST method route
+app.get("/test", (req, res) => {
+  res.json({user: "John", balance: "281402014"});
+});
+
+// POST method route
 app.post('/post', (req, res) => {
-    res.send('POST request to the homepage')
-    })
+  res.send('POST request to the homepage')
+})
 
 
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-    })
+  console.log(`Server listening on port ${port}`)
+})
